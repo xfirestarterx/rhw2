c27193e7add6c31bee6183e5fe96ced805c394e2
blah <blah>
2021-03-08 17:59:02 +0200

    task4 done

--- src/components/ModalDeleteMovie/ModalDeleteMovie.jsx
+++ src/components/ModalDeleteMovie/ModalDeleteMovie.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import Button, { buttonThemes } from '../Button/Button';
+import FormRow from '../FormRow/FormRow';
 import Modal from '../Modal/Modal';
 
 const ModalDeleteMovie = () => {
   return (
     <Modal title='delete movie'>
-      content
+      <p>Are you sure want to delete movie?</p>
+      <FormRow justifyContent='End'>
+        <Button theme={buttonThemes.confirm} text='confirm' propStyles={{maxWidth: '200px', marginTop: '20px'}} />
+      </FormRow>
     </Modal>
   );
 };
--- src/components/ModalEditMovie/ModalEditMovie.jsx
+++ src/components/ModalEditMovie/ModalEditMovie.jsx
@@ -1,12 +1,81 @@
 import React from 'react';
+import FormLabel from '../FormLabel/FormLabel';
+import FormRow, { flowAxisType } from '../FormRow/FormRow';
 import Modal from '../Modal/Modal';
+import TextField from '../TextField/TextField';
+import GenericInput from '../GenericInput/GenericInput';
+import LabeledDropdown, { containerWidth, dropdownTheme } from '../LabeledDropdown/LabeledDropdown';
+import Button, { buttonThemes } from '../Button/Button';
 
-const ModalAddMovie = () => {
+const getInputId = (name) => `modal-edit-input-${name}`;
+
+const inputs = {
+  title: getInputId('title'),
+  date: getInputId('date'),
+  movieUrl: getInputId('movie-url'),
+  overview: getInputId('overview'),
+  runtime: getInputId('runtime')
+}
+
+const genres = [
+  {
+    value: undefined,
+    text: 'Select genre'
+  },
+  {
+    value: 'action & adventure',
+    text: 'Action & Adventure'
+  },
+  {
+    value: 'music',
+    text: 'Music'
+  }
+];
+
+const ModalEditMovie = () => {
   return (
-    <Modal title='edit movie'>
-      content
+    <Modal title='Edit movie'>
+      <FormRow flowAxis={flowAxisType.y}>
+        <FormLabel text='movie id' />
+        <p style={{margin: 0}}>MO32820TH</p>
+      </FormRow>
+
+      <FormRow flowAxis={flowAxisType.y}>
+        <FormLabel attrFor={inputs.title} text='title' />
+        <TextField id={inputs.title} placeholder='Add title' />
+      </FormRow>
+
+      <FormRow flowAxis={flowAxisType.y}>
+        <FormLabel attrFor={inputs.date} text='release date' />
+        <GenericInput type='date' id={inputs.date} />
+      </FormRow>
+
+      <FormRow flowAxis={flowAxisType.y}>
+        <FormLabel attrFor={inputs.movieUrl} text='movie url' />
+        <TextField id={inputs.movieUrl} placeholder='Movie url here' />
+      </FormRow>
+
+      <FormRow flowAxis={flowAxisType.y}>
+        <FormLabel text='genre' />
+        <LabeledDropdown optionsList={genres} width={containerWidth.full} theme={dropdownTheme.dark} />
+      </FormRow>
+
+      <FormRow flowAxis={flowAxisType.y}>
+        <FormLabel attrFor={inputs.overview} text='overview' />
+        <TextField id={inputs.overview} placeholder='Overview here' />
+      </FormRow>
+
+      <FormRow flowAxis={flowAxisType.y}>
+        <FormLabel attrFor={inputs.runtime} text='runtime' />
+        <TextField id={inputs.runtime} placeholder='Runtime here' />
+      </FormRow>
+
+      <FormRow justifyContent='End'>
+        <Button theme={buttonThemes.dismiss} text='reset' propStyles={{ maxWidth: '100px' }} />
+        <Button theme={buttonThemes.confirm} text='submit' propStyles={{ maxWidth: '100px', marginLeft: '20px' }} />
+      </FormRow>
     </Modal>
   );
 };
 
-export default ModalAddMovie;
+export default ModalEditMovie;
