65a819954e4832f067ddba07db3c1332f32bd806
blah <blah>
2021-03-24 12:35:20 +0200

    added placeholder for missing images in movies list

--- src/components/CardImage/CardImage.jsx
+++ src/components/CardImage/CardImage.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import styles from './CardImage.styl';
 import PropTypes from 'prop-types';
 
-const CardImage = ({ img }) => <img src={img} />;
+const CardImage = ({ img }) => {
+  return (
+    <div className={styles.CardImageWrapper}>
+      <img src={img} />
+    </div>
+  );
+};
 
 CardImage.propTypes = {
   img: PropTypes.string.isRequired
