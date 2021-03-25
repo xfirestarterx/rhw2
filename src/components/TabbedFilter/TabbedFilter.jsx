6825515a1c80c4993885188cc43ad20e2349028c
blah <blah>
2021-03-25 12:32:08 +0200

    ability to switch filter tabs

--- src/components/TabbedFilter/TabbedFilter.jsx
+++ src/components/TabbedFilter/TabbedFilter.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styles from './TabbedFilter.styl';
 import TabItem from '../TabItem/TabItem';
@@ -6,9 +6,21 @@ import TabItem from '../TabItem/TabItem';
 const TabbedFilter = ({ items = [] }) => {
   if (!items?.length) return null;
 
+  const [itemsList, setItemsList] = useState(items);
+
+  const setActiveTab = (id) => {
+    const updatedList = itemsList.map(item => {
+      return item.id === id ?
+        { ...item, isActive: true } :
+        { ...item, isActive: false }
+    });
+
+    setItemsList(updatedList);
+  }
+
   return (
     <div className={styles.TabbedFilter}>
-      {items.map(({ id, title, isActive }) => <TabItem key={id} title={title} isActive={isActive} />)}
+      {itemsList.map(({ id, title, isActive }) => <TabItem onClick={setActiveTab} key={id} title={title} isActive={isActive} id={id} />)}
     </div>
   );
 };
